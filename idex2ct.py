1eb486df4ae0fb5b825953441dc0c3983d3df64d
ca-dmin <39862661+ca-dmin@users.noreply.github.com>
2018-06-03 09:08:10 +0200

    autopep8 formatting

--- idex2ct.py
+++ idex2ct.py
@@ -11,7 +11,8 @@ flagFeesAbziehen = True
 if len(sys.argv) == 1:
     print("Fehler! Bitte Dateiname uebergeben!")
     print("")
-    print("Usage: idex2ct.py <inputFile> [Y/N = Fees Abziehen vom Buy-Betrag; Default Y]  ")
+    print(
+        "Usage: idex2ct.py <inputFile> [Y/N = Fees Abziehen vom Buy-Betrag; Default Y]  ")
     print("")
     print("OutputFile ist <inputFile>._UploadCT.csv")
     print("")
@@ -25,24 +26,39 @@ if (len(sys.argv) > 2) and (sys.argv[2] == "N"):
     flagFeesAbziehen = False
 
 with open(outputFilename, 'w') as csvfile:
-    fieldnames = ['Type', 'Buy', 'CurB', 'Sell', 'CurS', 'Fee', 'CurF', 'Exchange', 'Group', 'Comment', 'Trade ID', 'Date']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator='\r\n')
+    fieldnames = [
+        'Type',
+        'Buy',
+        'CurB',
+        'Sell',
+        'CurS',
+        'Fee',
+        'CurF',
+        'Exchange',
+        'Group',
+        'Comment',
+        'Trade ID',
+        'Date']
+    writer = csv.DictWriter(
+        csvfile,
+        fieldnames=fieldnames,
+        lineterminator='\r\n')
     writer.writeheader()
     with open(inputFilename, 'r') as csvfile:
         inputReader = csv.DictReader(csvfile, delimiter='\t')
         for row in inputReader:
             # Defaults
-            Type='Trade'
-            Buy=''
-            CurB=''
-            Sell=''
-            CurS=''
+            Type = 'Trade'
+            Buy = ''
+            CurB = ''
+            Sell = ''
+            CurS = ''
             if (row['Type'] == 'Buy'):
-                  [Buy, CurB] = row['Amount'].split(' ');
-                  [Sell, CurS] = row['Total'].split(' ')
+                [Buy, CurB] = row['Amount'].split(' ')
+                [Sell, CurS] = row['Total'].split(' ')
             elif (row['Type'] == 'Sell'):
-                  [Sell, CurS] = row['Amount'].split(' ');
-                  [Buy, CurB] = row['Total'].split(' ')
+                [Sell, CurS] = row['Amount'].split(' ')
+                [Buy, CurB] = row['Total'].split(' ')
             # Fee
             Fee = 0.0
             GFee = 0.0
@@ -51,13 +67,13 @@ with open(outputFilename, 'w') as csvfile:
                 Fee = float(row['Fee'].split(' ')[0])
             except ValueError as e:
                 Fee = 0.0
-                #print(e.message)
+                # print(e.message)
 
             try:
                 GFee = float(row['Gas Fee'].split(' ')[0])
             except ValueError as e:
                 GFee = 0.0
-                #print(e.message)
+                # print(e.message)
             # Fee = Fee + Gas Fee
             Fee += float(GFee)
 
@@ -67,24 +83,13 @@ with open(outputFilename, 'w') as csvfile:
             if CurF != CurB:
                 print("FEHLER!?!?! CurF != CurB?")
 
-            Exchange='Idex'
-            Group=''
-            Comment=''
-            TradeID=row['Transaction ID']
-            Date=row['Date']
+            Exchange = 'Idex'
+            Group = ''
+            Comment = ''
+            TradeID = row['Transaction ID']
+            Date = row['Date']
 
             writer.writerow({
-            'Type': Type
-            , 'Buy': Buy
-            , 'CurB': CurB
-            , 'Sell': Sell
-            , 'CurS': CurS
-            , 'Fee': Fee
-            , 'CurF': CurF
-            , 'Exchange': Exchange
-            , 'Group': Group
-            , 'Comment': Comment
-            , 'Trade ID': TradeID
-            , 'Date': Date })
+                'Type': Type, 'Buy': Buy, 'CurB': CurB, 'Sell': Sell, 'CurS': CurS, 'Fee': Fee, 'CurF': CurF, 'Exchange': Exchange, 'Group': Group, 'Comment': Comment, 'Trade ID': TradeID, 'Date': Date})
 
 print("Done: " + inputFilename + " --> " + outputFilename + " erstellt.")
